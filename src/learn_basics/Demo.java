44a15fce627039c8deedd6cd150e48663d6e0a4c
Shivanand-76 <birajdarshiva84@gmail.com>
2025-03-10 22:52:46 +0530

    add basics of java

---
+++ src/learn_basics/Demo.java
@@ -0,0 +1,9 @@
+package learn_basics;
+
+public class Demo {
+    public static void main(String[]args) {
+    	System.out.println("Hello World");
+    	System.out.print("How are you??");
+    	System.out.println("I am fine what about you");
+    }
+}
